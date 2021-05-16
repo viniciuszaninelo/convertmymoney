4bea94cab11ca35bc91660b722c736ee8fa4c2ab
Vinícius Marques Zaninelo <vinicousmarqueszaninelo@gmail.com>
2021-05-16 15:34:25 -0300

    FINALIZAÇÃO

--- index.js
+++ index.js
@@ -1,27 +1,31 @@
 const express = require('express')
 const app = express()
-const path = require('path') //COMO NO FUTURO IREI POSTAR/ APLICAR O PROJETO, EU PRECISO FAZER O TRATAMENTO DO 'PATH', PARA QUE ELE FUNCIONE 
+const path = require('path')
 const convert = require('./lib/convert')
 
 // PARA JA APPARECER A TELA DO FORMULARIO NA TELA, PRECISAMOS UAR O EJS, E PARA USAR O EJS, PRECISO SETAR MEU VIEW ENGINE
 app.set('view engine', 'ejs')
-//VAMOS MOSTRAR AQUI QUAL DIRETORIO VAI TER OS MEUS VIEWS
 app.set('views', path.join(__dirname, 'views')) // 'path.join' vai criar um diretorio para mim, ai ele vai pega o diretorio atual(DIRNAME) e somar com o meu 'views'   
 app.use(express.static(path.join(__dirname, 'public'))) // Aqui é um lugar para colocar os nossos arquivos, (css, fotos , etc...)  -> NA PASTA PUBLIC VAI TA OS NOSSOS ARQUIVOS
-//  '/' é a parte principal
-app.get('/', (req, res) => { // request e response 
-  res.render('home')  // estou renderizando nossa página  'home' 
+app.get('/', (req, res) => {
+  res.render('home')
 })
 
 app.get('/cotacao', (req, res) => {
   const { cotacao, quantidade } = req.query
-  // ja consigo fazer a conversao 
-  const conversao = convert.convert(cotacao, quantidade)
-  res.render('cotacao', {
-    cotacao,
-    quantidade,
-    conversao
-  })
+  if (cotacao && quantidade) { // para se o usuario digitar nda
+    const conversao = convert.convert(cotacao, quantidade)
+    res.render('cotacao', {
+      error: false,
+      cotacao: convert.toMoney(cotacao),
+      quantidade: convert.toMoney(quantidade),
+      conversao: convert.toMoney(conversao)
+    })
+  } else {
+    res.render('cotacao', {
+      error: 'Os valores a seguir são inválidos'
+    })
+  }
 })
 
 app.listen(3000, err => { // retorne um erro ce nao for possivel abrir a porta 3000
